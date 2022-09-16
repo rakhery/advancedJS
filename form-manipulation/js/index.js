fe60d74460228de36cbdf01d3658f18096de86ea
Christian RAKOTO <crakoto@yahoo.fr>
2022-09-16 11:26:56 +0200

    Correction Formateur, il y a deux méthodes ForeaCh  (Raisa)

--- form-manipulation/js/index.js
+++ form-manipulation/js/index.js
@@ -5,36 +5,25 @@ const phoneNumber=document.querySelector("#phone_number");
 const country=document.querySelector("#country");
 const submitButton=document.querySelector("#submit_button");
 const usersTableBody=document.querySelector("#users_list");
+const formFields = [firstName, lastName, email, phoneNumber, country];
 
 submitButton.addEventListener("click", function (event) {
     event.preventDefault();
     const row = document.createElement("tr");
-    const firstNameColumn = document.createElement("td");
-    firstNameColumn.innerText = firstName.value;
 
-    const lastNameColumn = document.createElement("td");
-    lastNameColumn.innerText = lastName.value;
-
-    const emailColumn = document.createElement("td");
-    emailColumn.innerText = email.value;
-
-    const phoneNumberColumn = document.createElement("td");
-    phoneNumberColumn.innerText = phoneNumber.value;
-
-    const countryColumn = document.createElement("td");
-    countryColumn.innerText = country.value;
-    row.append(
-        lastNameColumn,
-        firstNameColumn,
-        emailColumn,
-        countryColumn,
-        phoneNumberColumn
-    );
+    for (const field of formFields) {
+        const column = CreateColumn(field.value);
+        row.appendChild(column);
+    }
 
     usersTableBody.appendChild(row);
 });
 
-
+function CreateColumn(textContent) {
+    const column = document.createElement("td");
+    column.innerText = textContent;
+    return column;
+}
 
 
 
