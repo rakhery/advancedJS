ecc36be00ecc81c27bff640bbd65831873fb2a54
Christian RAKOTO <crakoto@yahoo.fr>
2022-09-16 10:46:54 +0200

    add eventListner

--- form-manipulation/js/index.js
+++ form-manipulation/js/index.js
@@ -4,11 +4,37 @@ const email=document.querySelector("#email");
 const phoneNumber=document.querySelector("#phone_number");
 const country=document.querySelector("#country");
 const submitButton=document.querySelector("#submit_button");
+const usersTableBody=document.querySelector("#users_list");
 
-submitButton.addEventListener("click",function(event){
+submitButton.addEventListener("click", function (event) {
     event.preventDefault();
-    console.log("Formulaire envoyé!!")
+    const row = document.createElement("tr");
+    const firstNameColumn = document.createElement("td");
+    firstNameColumn.innerText = firstName.value;
+
+    const lastNameColumn = document.createElement("td");
+    lastNameColumn.innerText = lastName.value;
+
+    const emailColumn = document.createElement("td");
+    emailColumn.innerText = email.value;
+
+    const phoneNumberColumn = document.createElement("td");
+    phoneNumberColumn.innerText = phoneNumber.value;
+
+    const countryColumn = document.createElement("td");
+    countryColumn.innerText = country.value;
+
+    row.append(
+        lastNameColumn,
+        firstNameColumn,
+        emailColumn,
+        countryColumn,
+        phoneNumberColumn
+    );
+
+    usersTableBody.appendChild(row);
 });
+
 
 
 
